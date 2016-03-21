48471792ea9ec50277a934d86e155ec4ebaea0c8
Jesse Reynolds <jesse@va.com.au>
2016-03-21 19:54:11 +1030

    Start managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,13 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}, and I am a happy little vegemite": }
+  
+  file {'/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'We learned all the things',
+  }
+  
 }
